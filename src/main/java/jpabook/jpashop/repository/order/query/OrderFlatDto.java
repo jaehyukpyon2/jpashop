2b148d0baa09dc41de5bcde2d037633a5ff3989b
jaehyukpyon2 <ipadforjay@gmail.com>
2024-05-20 16:43:15 +0900

    end of day

---
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.repository.order.query;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+public class OrderFlatDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private OrderStatus status;
+    private Address address;
+
+    private String itemName;
+    private int orderPrice;
+    private int count;
+}
